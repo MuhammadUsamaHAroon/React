251b16381cd9cd631a49dc0c17873d8bf67a7aaa
usama haroon <muhammadusamaharoon13579@gmail.com>
2021-03-14 01:46:17 +0500

    Assigment 4

--- src/parent.js
+++ src/parent.js
@@ -11,25 +11,43 @@ const arrObj2 = [{user:"Junaid",age:25},{user:"jawwad",age:20},{user:"Addel",age
 function Parent(){
     // let title1 = "MY Name is Usama"
     // let title2 = "My Name is Ammar"
-    const [bulb, setBulb] = useState(false)
+    // const [bulb, setBulb] = useState(false)
+    const [count, setCount]=useState(0)
+
+    function IncreamentCount(){
+        setCount( count + 1)
+    }
+
+    function DecreamentCont(){
+    //    var zeroLimit = count>=0
+        // if(count===0){
+        //     alert("Sorry!!")
+        // }
+        setCount(count - 1)
+        
+    }
+
+
+    
     
 
     return(
     
         
-        <div>
+        <>
            
-           
-
-            {/* <FirstChild arra1={arrObj1}/>
-
-        
+           <button onClick={IncreamentCount}>Increament</button>
+           <span>{count}</span>
+           <button onClick={DecreamentCont}>Decreament</button>
+            {/* <FirstChild arra1={arrObj1}/
             <SecondChild arra2={arrObj2} /> */}
 
-            <h1>{bulb === true ? <img style={{width:"15.5%"}} src={OnBulb} alt="Image not found" />:""}</h1>
+            {/* <h1>{bulb === true ? <img style={{width:"15.5%"}} src={OnBulb} alt="Image not found" />:""}</h1>
             <h1>{bulb === false?<img style={{width:"20%"}} src={OffBulb} alt="Image not found"/>:"" }</h1>
-            <button onClick={()=>bulb === true ? setBulb(false):setBulb(true)}>{bulb === false  ? "Open" : "Close"}</button>
-        </div>
+            <button onClick={()=>bulb === true ? setBulb(false):setBulb(true)}>{bulb === false  ? "Open" : "Close"}</button> */}
+
+            
+        </>
     
     )
 }
