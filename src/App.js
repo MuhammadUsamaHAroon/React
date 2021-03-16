2bc184f2a14a5f04107cccfcb6829191178a8f67
usama haroon <muhammadusamaharoon13579@gmail.com>
2021-03-17 02:06:41 +0500

    Assigment 5

--- src/App.js
+++ src/App.js
@@ -4,37 +4,84 @@ import React, {useState} from 'react';
 import Parent from './parent'
 
 function App() {
-  const [user, setUser] = useState ("")
-  const [naam, setNaam] = useState(false)
-  const val = function(e){
-    const input = e.target.value
-    console.log(input)
-    setUser(input)
-    
-    
-  }
-   const getName = localStorage.getItem("UserName", user)
-   function show() {
+  //Assigment
+// using state
+// email = admin@domain.com
+// Password = abc123
+// Show login Successfull
+const tittle = "Log In"
+const [email, setEmail] = useState("admin@domain.com")
+const [userEmail, setUserEmail] = useState("")
+const [password, setPassword] = useState("abc123")
+const [userpassword, setUserPassword] = useState("")
 
-    const show = getName
-    console.log(show)
-    setNaam(true)
+
+
+  // const [user, setUser] = useState ("")
+  // const [naam, setNaam] = useState(false)
+  // const val = function(e){
+  //   const input = e.target.value
+  //   console.log(input)
+    // setUser(input)
+    
+    
+  // }
+  //  const getName = localStorage.getItem("UserName", user)
+  //  function show() {
+
+  //   const show = getName
+  //   console.log(show)
+  //   setNaam(true)
      
-   }
+  //  }
    
-  const submit = function(){
-    console.log("State==>"+user)
-    localStorage.setItem("UserName", user)
-  }
+  // const submit = function(){
+  //   console.log("State==>"+user)
+  //   localStorage.setItem("UserName", user)
+  // }
     // let number= 1;
+    // const submit = function () {
+    // }
+    const showValue = function () {
+      console.log(userEmail)
+      console.log(email)
+      console.log(password)
+      console.log(userpassword)
+      
+    }
+    const emailValue = function(e){
+      const input = e.target.value
+      console.log(input)
+      setUserEmail(input)
+    }
 
+    const passwordValue = function(e){
+      const inputPassword = e.target.value
+      console.log(inputPassword)
+      setUserPassword(inputPassword)
+    }
+    
   return (
     <div>
+      <h1>{tittle}</h1><br/>
+      <input placeholder = "Email"onChange = {emailValue}/><br/><br/>
+      <input placeholder = "Password"onChange = {passwordValue}/><br/><br/>
+      <button onClick={(showValue)=> email === userEmail && password === userpassword? alert("Login Successfully"):alert("failed")}>Login</button>
 
-    <input onChange={val}/>
+
+      {/* <input placeholder={Email}/> */}
+
+
+    {/* <input onChange={val}/>
     <button onClick={submit}>Submit</button>
     <button onClick={show}>Show</button>
-    {naam ? <ul><li>{getName}</li></ul> :"" }
+    {naam ? <ul><li>{getName}</li></ul> :"" } */}
+
+      {/* <input placeholder="Email" onChange={inputValue}/>
+      {<button>Submit</button>} 
+  //  }
+
+    
       {/* <Parent/> */}
       {/* <h1>Hello I am  App</h1> */}
 
@@ -48,4 +95,4 @@ function App() {
   }
   
 
-export default  App;
+export default  App
--- src/parent.js
+++ src/parent.js
@@ -4,38 +4,45 @@ import SecondChild from './SecondChild'
 import OffBulb from "./Asset/images/OIP.jpeg"
 import OnBulb from "./Asset/images/yellow_bulb.png"
 
-const arrObj1 = [{user:"Arshaq",age:25},{user:"Areeb",age:20},{user:"Arif",age:22}]
-const arrObj2 = [{user:"Junaid",age:25},{user:"jawwad",age:20},{user:"Addel",age:22}]
+// const arrObj1 = [{user:"Arshaq",age:25},{user:"Areeb",age:20},{user:"Arif",age:22}]
+// const arrObj2 = [{user:"Junaid",age:25},{user:"jawwad",age:20},{user:"Addel",age:22}]
 // console.log(arrObj1
 
 function Parent(){
+   
     // let title1 = "MY Name is Usama"
     // let title2 = "My Name is Ammar"
     // const [bulb, setBulb] = useState(false)
-    const [count, setCount]=useState(0)
+    // const [count, setCount]=useState(0)
 
-    function IncreamentCount(){
-        setCount(count + 1)
-    }
+    // function IncreamentCount(){
+    //     setCount(count + 1)
+    // }
 
-    function DecreamentCont(){
-    //    var zeroLimit = count>=0
-        // if(count===0){
-        //     alert("Sorry!!")
-        // }
-        setCount(count - 1)
+    // function DecreamentCont(){
+    // //    var zeroLimit = count>=0
+    //     // if(count===0){
+    //     //     alert("Sorry!!")
+    //     // }
+    //     setCount(count - 1)
         
-    }
+    // }
+    
+
+    
+
 
 
     
     
 
     return(
-    
         
-        <>
+        
+        <div>
+          
            
+
            {/* <button onClick={IncreamentCount}>Increament</button>
            <span>{count}</span>
            <button onClick={DecreamentCont}>Decreament</button> */}
@@ -47,7 +54,7 @@ function Parent(){
             <button onClick={()=>bulb === true ? setBulb(false):setBulb(true)}>{bulb === false  ? "Open" : "Close"}</button> */}
 
             
-        </>
+        </div>
     
     )
 }
